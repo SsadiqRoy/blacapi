87ef4fbbd372f8ce329db1e42721bff9f624f3b8
eventeos <ssadiqrou@eventeos.com>
2022-09-28 12:52:03 +0000

    changes

--- db.js
+++ db.js
@@ -1,14 +1,34 @@
-const { Sequelize } = require("sequelize");
+const { Sequelize } = require('sequelize');
 
-const { db_name, db_password, db_user, db_host } = process.env;
+/**
+ * gets environment variables for connecting with the database depending on localhost or online
+ * and if online depending on beta or master
+ * @returns Object of Objects form process env
+ */
+function getConfigOptions() {
+  // retrurning evironment variables depending on localhost
+  if (process.env.platform === 'local') {
+    const { dbl_name, dbl_password, dbl_user, dbl_host, dbl_port, dbl_dialect } = process.env;
+    return {
+      option1: { name: dbl_name, password: dbl_password, user: dbl_user },
+      option2: { host: dbl_host, port: +dbl_port, dialect: dbl_dialect },
+    };
+  }
+  // retrurning evironment variables depending on online
+  if (process.env.platform === 'online') {
+    const { DB_NAME, DB_BETA_NAME, DB_PASSWORD, DB_USER, DB_HOST, DB_PORT, DB_DIALECT, runon } = process.env;
+    return {
+      option1: { name: runon === 'beta' ? DB_BETA_NAME : DB_NAME, password: DB_PASSWORD, user: DB_USER },
+      option2: { host: DB_HOST, port: +DB_PORT, dialect: DB_DIALECT },
+    };
+  }
+}
 
-// console.log(db_name, db_password, db_user, host);
+// getting variables for connections
+const { option1, option2 } = getConfigOptions();
+const { name, password, user } = option1;
 
-const sequelize = new Sequelize(db_name, db_user, db_password, {
-  host: db_host,
-  dialect: "mysql",
-  // dialect: "mariadb",
-  port: 3306,
-});
+// connectiong to the database
+const sequelize = new Sequelize(name, user, password, option2);
 
 module.exports = sequelize;
--- server.js
+++ server.js
@@ -1,35 +1,33 @@
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 
-process.on("uncaughtException", (error) => {
+process.on('uncaughtException', (error) => {
   console.log(error);
   process.exit(1);
 });
 
-dotenv.config({ path: "./config.env" });
-// const { host, port, db_name, db_user, db_password } = process.env;
-// console.log(db_user, db_name, db_password, host);
+dotenv.config({ path: './config.env' });
 
 // connecting to the database
-const sequelize = require("./db");
+const sequelize = require('./db');
 async function connectDB() {
   try {
     await sequelize.authenticate();
     await sequelize.sync();
     // sequelize.sync({ alter: true });
-    console.log("connection successfull");
+    console.log('connection successfull');
   } catch (error) {
-    console.log("DB_CONNECTION🔥", error);
+    console.log('DB_CONNECTION🔥', error);
   }
 }
 connectDB();
 
 // running the app
-const app = require("./app");
+const app = require('./app');
 
 // creating server for the app
-const server = app.listen(process.env.port, process.env.host, () => console.log("we are listenning..."));
+const server = app.listen(process.env.port, process.env.host, () => console.log('we are listenning...'));
 
-process.on("unhandledRejection", (error) => {
+process.on('unhandledRejection', (error) => {
   console.log(error);
   server.close(() => {
     process.exit(1);
