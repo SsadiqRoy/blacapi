73dcbacea0230bf6642cd160969b79359ec0d81d
eventeos <ssadiqrou@eventeos.com>
2022-10-27 14:35:37 +0000

    ch

--- utils/errors.js
+++ utils/errors.js
@@ -3,17 +3,17 @@ const { updater } = require('./utils');
 
 exports.globalError = (error, req, res, next) => {
   error.oldmessage = error.message;
-
-  // sending reasonable error in jwt token expery
-  if (error.name === 'TokenExpiredError') error.message = 'please log in again';
-
-  console.log(error);
   updater(async () => {
     const errors = JSON.parse(await fs.readFile(`./errors/error.js`));
     error.date = date.now();
     error.push(error);
     fs.writeFile('./errors/error.json', JSON.stringify(errors));
   });
+  // sending reasonable error in jwt token expery
+  if (error.name === 'TokenExpiredError') error.message = 'please log in again';
+
+  console.log(error);
+
   res.status(500).json({
     status: 'failed',
     message: error.message,
